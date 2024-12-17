897bb0b780dd8a54ecf5c410d5788eff38160832
sait-amm <sait-amm@c4r2p9.1337.ma>
2024-12-17 11:02:00 +0100

    angl ei think is right ??????

--- draw_map/move_player.c
+++ draw_map/move_player.c
@@ -3,18 +3,22 @@
 void move_player(t_data *data)
 {
     data->mlx.image_addr = mlx_get_data_addr(data->mlx.image, &data->mlx.bits_per_pixel, &data->mlx.line_length, &data->mlx.endian);
-<<<<<<< HEAD
-=======
-    // printf("--->%f--->%f\n", data->map.p.p_y , data->map.p.p_x);
->>>>>>> origin/parsing
     if (data->map.map[(int)data->map.p.p_y / 64][(int)data->map.p.p_x / 64] == '1' ) 
         return;
     draw_map(data);
     mlx_put_image_to_window(data->mlx.mlx, data->mlx.window, data->mlx.image, 0, 0);
 }
 
-
-
+int no_event(t_data *data)
+{
+    if (data->map.map[(int)data->map.p.p_y / 64][(int)data->map.p.p_x / 64] == '1' || !data->map.map[(int)data->map.p.p_y / 64][(int)data->map.p.p_x / 64])
+            return (SUCCESS);
+    if (data->map.map[(int)data->map.p.p_y / 64][(int)data->map.p.p_x / 64] == '1' || !data->map.map[(int)data->map.p.p_y / 64][(int)data->map.p.p_x / 64])
+            return (SUCCESS);
+    if (!data->map.p.p_y)
+        return (SUCCESS);
+    return (FAILURE);
+}
 int handle_key(int keycode, t_data *data)
 {
     if (keycode == ESC)
